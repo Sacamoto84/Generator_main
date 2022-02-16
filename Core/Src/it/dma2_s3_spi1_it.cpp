4d1cca7d9055c65e381fa970abd9cd18cbf150f9
Ivan <zorromen@mail.ru>
2022-02-16 20:54:29 +0300

    Перенос библиотек, изменение в it DMA. добавил описалово

--- Core/Src/it/dma2_s3_spi1_it.cpp
+++ Core/Src/it/dma2_s3_spi1_it.cpp
@@ -1,10 +1,3 @@
-/*
- * dma2_s3_spi1.cpp
- *
- *  Created on: 19 но€б. 2021 г.
- *      Author: Ivan
- */
-
 #include "main.h"
 #include "TFT.h"
 #include "dma.h"
@@ -14,14 +7,11 @@ extern TFT tft CCMRAM;
 
 extern "C" void DMA2_Stream3_IRQHandler(void)
 {
-
 	if (DMA2->LISR & DMA_LISR_TCIF3)
 		      tft.DMA_TX_Complete = 1;
 
-	if (DMA2->LISR & DMA_LISR_FEIF3)
-			      tft.DMA_TX_Complete = 1;
+	//if (DMA2->LISR & DMA_LISR_FEIF3)
+	//		      tft.DMA_TX_Complete = 1;
 
   HAL_DMA_IRQHandler(&hdma_spi1_tx);
 }
-
-
